044ce2d34ee11fd548a5c482abd29ea6b1ac01f0
dediane <dianedecourt@gmail.com>
2024-05-21 18:35:10 +0200

    y

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,9 +1,22 @@
+import { Navbar } from "@/components/_navbar";
 import React from "react";
+import styles from "@/styles/contactSection.module.css";
+
+const ContactSection = () => {
+    return (
+        <>
+            <div className={styles.container}>
+            <h1>Contact</h1>
+            </div>
+        </>
+    )
+}
 
 export default function Contact () {
     return (
         <>
-        <p>Contact - work in process</p>
+        <Navbar />
+        <ContactSection />
         </>
     )
 }
